a8ee023210d586f12156e83ea89d19cfe8fb0223
Chinedu okere <okere360@gmail.com>
2020-04-17 19:46:04 +0100

    broken our homepage into smaller components, finished styling the menu items

---
+++ online/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
+
+function App() {
+  return (
+    <div>
+      <h1><HomePage /></h1>
+    </div>
+  );
+}
+
+export default App;
