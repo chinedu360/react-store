2ff07e7ef0295dd203da471af6319db724c36546
Chinedu okere <okere360@gmail.com>
2020-04-18 17:28:43 +0100

    update our menu-items to navigate withRouter HOC

--- online/src/components/directory/directory.component.jsx
+++ online/src/components/directory/directory.component.jsx
@@ -14,29 +14,34 @@ class Directory extends React.Component {
                 {
                     title: 'hats',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id: 1
+                    id: 1,
+                    linkUrl: 'hats'
                 },
                 {
                     title: 'jackets',
                     imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                    id: 2
+                    id: 2,
+                    linkUrl: ''
                 },
                 {
                     title: 'sneakers',
                     imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                    id: 3
+                    id: 3,
+                    linkUrl: ''
                 },
                 {
                     title: 'hats',
                     imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                     size: 'large',
-                    id: 4
+                    id: 4,
+                    linkUrl: ''
                 },
                 {
                     title: 'hats',
                     imageUrl: 'https://cdn.pixabay.com/photo/2016/11/23/18/17/adult-1854191__340.jpg',
                     size: 'large',
-                    id: 5
+                    id: 5,
+                    linkUrl: ''
                 },
             ]
         }
@@ -46,8 +51,8 @@ class Directory extends React.Component {
         return (
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({title, imageUrl, id, size}) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                    this.state.sections.map(({id, ...otherSectionProps }) => (
+                        <MenuItem key={id} {...otherSectionProps} />
                     ))
                 }
             </div>
